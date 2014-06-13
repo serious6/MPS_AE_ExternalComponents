2b3b124f3b2cf5860b4069466e1f3c5f6eb57880
serious6 (andre.behrens@haw-hamburg.de) <andre.behrens@haw-hamburg.de>
2014-06-13 17:04:52 +0200

    Added error-handler

--- MPS_AE_Transportdienstleister/src/main/java/de/hawhamburg/mps/transport/controller/TransportAdapterController.java
+++ MPS_AE_Transportdienstleister/src/main/java/de/hawhamburg/mps/transport/controller/TransportAdapterController.java
@@ -2,14 +2,18 @@ package de.hawhamburg.mps.transport.controller;
 
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import de.hawhamburg.mps.transport.service.TransportService;
 import de.hawhamburg.mps.transport.transObjects.TransportObject;
@@ -43,6 +47,16 @@ public class TransportAdapterController {
 		logger.info("TransportService is "
 				+ (transportService != null ? "running" : "offline"));
 		return "Running...";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public @ResponseBody
+	ModelAndView errorHandler(HttpServletRequest req, Exception exception) {
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("exception", exception);
+		modelAndView.addObject("url", req.getRequestURL());
+		modelAndView.setViewName("error");
+		return modelAndView;
 	}
 
 }
